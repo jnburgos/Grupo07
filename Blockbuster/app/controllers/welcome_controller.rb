7cebae46859d2fb7ecfdc3241e5fa9570c1e9ef5
EDUARDO GUERRA <eeguerra@uc.cl>
2017-05-18 17:46:31 -0400

    Made shows page the main page

--- Blockbuster/app/controllers/welcome_controller.rb
+++ Blockbuster/app/controllers/welcome_controller.rb
@@ -1,9 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    if user_signed_in?
-      redirect_to shows_path
-    else
-      redirect_to new_user_session_path
-    end
+    # if user_signed_in?
+    #   redirect_to shows_path
+    # else
+    #   redirect_to new_user_session_path
+    # end
+    redirect_to shows_path
   end
 end
